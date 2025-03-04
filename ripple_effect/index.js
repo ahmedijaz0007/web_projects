3f00884d490536902d0fcc8e9fe9a566b62e7b71
ahmedijaz <ahmedijaz0007@gmail.com>
2025-03-03 23:27:32 -0800

    rippleEffect Completed

---
+++ ripple_effect/index.js
@@ -0,0 +1,10 @@
+const btnEl = document.querySelector('#ripple-btn');
+
+btnEl.addEventListener("mouseover", (event) => {
+    const x = (event.pageX - btnEl.offsetLeft);
+    const y = (event.pageY - btnEl.offsetTop);
+    console.log(x)
+    console.log(y)
+    btnEl.style.setProperty('--xPos',x + 'px');
+    btnEl.style.setProperty('--yPos',y + 'px')
+})
