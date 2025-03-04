17adf5c3568a06f7f1f4d2e7754d4cb06b4126be
ahmedijaz <ahmedijaz0007@gmail.com>
2025-03-03 21:16:39 -0800

    text animation improved

--- text_animation/index.js
+++ text_animation/index.js
@@ -1,10 +1,30 @@
-const texts = ['I am a Freelancer', 'I am a Software Engineer', 'I am a Full-Stack Developer'];
+const careers = ['Freelancer', 'Software Engineer', 'Full-Stack Developer'];
+const vowels = ['a','e','i','o','u','A','E','I','O','U'];
 
 const text = document.querySelector('#Main-text')
 
-let index = 0;
-setInterval(() => {
-    index++;
-    index = index % texts.length;
-    text.textContent = texts[index];
-}, 3000)
+let careerIndex = 0;
+let charIndex = 0;
+
+//swapText();
+updateText();
+
+function swapText(){
+    setInterval(() => {
+        careerIndex++;
+        careerIndex = careerIndex % careers.length;
+        text.innerHTML = careers[careerIndex];
+    }, 3000)
+}
+
+
+function updateText(){
+    text.innerHTML = `<h1>I am ${vowels.includes(careers[careerIndex].slice(0,1)) ? 'an':'a'} ${careers[careerIndex].slice(0,charIndex)}<h1>`;
+    charIndex++;
+    if(charIndex > careers[careerIndex].length){
+        charIndex = 0;
+        careerIndex ++;
+        careerIndex = careerIndex % careers.length;
+    }
+    setTimeout(updateText,100)
+}
