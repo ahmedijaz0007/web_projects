edd5cca5efee90dcdc9d5fb21dea72351b7e72ee
ahmedijaz <ahmedijaz0007@gmail.com>
2025-03-04 22:28:04 -0800

    ch counter done

---
+++ character_counter/index.js
@@ -0,0 +1,18 @@
+const textArea = document.querySelector(".text-input");
+const remainingEl = document.querySelector(".remaining-ch");
+const totalEl = document.querySelector(".total-ch");
+
+let totalChs = 0;
+const allowedCh = 50;
+let remainingChs = 50;
+
+textArea.addEventListener('input', () => {
+    console.log(textArea.value); // Logs the updated text
+    textArea.value = textArea.value.slice(0,allowedCh);
+    remainingChs = allowedCh - textArea.value.length;
+    totalChs = textArea.value.length;
+    remainingEl.innerText = remainingChs;
+    totalEl.innerText = totalChs;
+    
+
+})
